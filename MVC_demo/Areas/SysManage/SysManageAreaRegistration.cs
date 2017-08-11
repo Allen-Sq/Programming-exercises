08c4f62b0c804f1a8d8fd16df5b8d0d99e5f654e
Allen Su <allen_su0611@126.com>
2017-08-11 17:09:42 +0800

    添加项目文件。

---
+++ MVC_demo/Areas/SysManage/SysManageAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace MVC_demo.Areas.SysManage
+{
+    public class SysManageAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "SysManage";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "SysManage_default",
+                "SysManage/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                new string[] { "MVC_demo.Areas.SysManage.Controllers" }
+
+            );
+        }
+    }
+}
---
+++ MVC_demo/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        //my first mvc knockoutJS
+        public ActionResult First()
+        {
+            return View();
+        }
+        //监控属性
+        public ActionResult Second()
+        {
+            return View();
+        }
+
+        public ActionResult Third()
+        {
+            return View();
+        }
+
+        public ActionResult Four()
+        {
+            return View();
+        }
+
+        public ActionResult Fifth()
+        {
+            return View();
+        }
+    }
+}
